361d45520496cbfb9292921224b7f093dc5efdb5
tjddus <rlatjddus5767@naver.com>
2023-11-13 16:47:34 +0900

    dart 버전 문제

--- lib/search_map.dart
+++ lib/search_map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 
 class Search_Map extends StatefulWidget {
   const Search_Map({super.key});
@@ -10,7 +11,17 @@ class Search_Map extends StatefulWidget {
 class _Search_MapState extends State<Search_Map> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(); //어려워보이네
+    return const MaterialApp(
+        home: Scaffold(
+      body: Column(
+        children: [
+          Text(
+            '원하시는 위치를 검색해보세요!',
+            style: TextStyle(fontSize: 0),
+          ),
+        ],
+      ),
+    )); //어려워보이네
   }
 }
 
